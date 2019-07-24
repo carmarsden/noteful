53e4e1d602b7326cc274e0f9b3df01c708abdd47
Caroline Marsden <car.marsden@gmail.com>
2019-07-24 19:59:26 -0400

    Update scripts for deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-    API_SERVER: `http://localhost:8000`,
-    API_KEY: process.env.REACT_APP_API_KEY,
-    API_TOKEN: 'blahblah'
+    API_SERVER: `https://frozen-cliffs-87082.herokuapp.com`,
+    API_KEY: process.env.REACT_APP_API_KEY
 }
